b0a611edc32532218d6ed48b1079a8e28f9d1627
Sofaaaaaaaaa <sofat3026@gmail.com>
2024-10-01 16:25:02 +0300

    srez

---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+'use strict';
+
+
+ 
+let currentIndex = 0; 
+const slides = document.querySelectorAll(".slide"); 
+const dots = document.querySelectorAll(".dot"); 
+ 
+function showSlide(index) { 
+    if (index >= slides.length) { 
+        currentIndex = 0; 
+    } else if (index < 0) { 
+        currentIndex = slides.length - 1; 
+    } else { 
+        currentIndex = index; 
+    } 
+ 
+    slides.forEach((slide, i) => { 
+        slide.style.display = "none"; 
+        dots[i].classList.remove("active"); 
+    }); 
+ 
+    slides[currentIndex].style.display = "block"; 
+    dots[currentIndex].classList.add("active"); 
+} 
+ 
+ 
+function moveSlide(step) { 
+    showSlide(currentIndex + step); 
+} 
+ 
+function currentSlide(index) { 
+    showSlide(index); 
+} 
+ 
+function initSlider() { 
+    showSlide(currentIndex); 
+} 
+ 
+dots.forEach((dot, index) => { 
+    dot.addEventListener("click", () => currentSlide(index)); 
+}); 
+ 
+document.addEventListener("DOMContentLoaded", initSlider);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var menuItems = document.querySelectorAll('.menu > li');
+
+    menuItems.forEach(function (item) {
+        item.addEventListener('mouseenter', function () {
+            this.classList.add('active');
+        });
+
+        item.addEventListener('mouseleave', function () {
+            this.classList.remove('active');
+        });
+    });
+});
+
+
+document.querySelector('.faq').addEventListener('click', function(event){
+    let target = event.target.closest('.faq__item');
+    if(!target) return;
+
+    target.classList.toggle('active');
+    let p = target.querySelector('p');
+    if(target.classList.contains('active')){
+        p.style.height = p.scrollHeight + 'px';
+    }else{
+        p.style.height = '';
+    }
+})
+
+var modal = document.getElementById("myModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+window.onload = function () {
+  setTimeout(function () {
+    modal.style.display = "block";
+  }, 3000);
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+
+// TABS start 
+ 
+let tabBtns = document.querySelectorAll('.but') 
+let tabTexts = document.querySelectorAll('.cards') 
+ 
+tabBtns.forEach((item, index)=>{ 
+    item.addEventListener('click', function(){ 
+        document.querySelector('.but.active').classList.remove('active'); 
+        item.classList.add('active'); 
+ 
+        document.querySelector('.cards.active').classList.remove('active'); 
+        tabTexts[index].classList.add('active') 
+    }) 
+}) 
+ 
+// TABS end
+
+
+
+
+"use strict";
+let phoneBtn = document.querySelector(".phone");
+let sale = document.querySelector(".fag");
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        phoneBtn.classList.add("show");
+      } else {
+        phoneBtn.classList.remove("show");
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+observer.observe(sale);
+
+
